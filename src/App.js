de059796856f967e2b35af603a7d6e4de4014859
vaibhavtiwari0112 <vaibhavtiwari0128@gmail.com>
2022-11-14 19:17:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Pathfindervisualizer from './pathfinder/pathfindervisualizer.jsx'
+
+function App() {
+  return (
+    <div className="App">
+    <Pathfindervisualizer></Pathfindervisualizer>
+    </div>
+  );
+}
+
+export default App;
